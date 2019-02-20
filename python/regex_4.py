d38f012c033b02149ba5cfe5f1544457e3e4b2f8
UsuarioTemporal <thomtwd@gmail.com>
2019-02-20 17:12:33 -0500

    Regex con json

--- python/regex_4.py
+++ python/regex_4.py
@@ -1,2 +1,13 @@
 import re
+import json
 
+with open('data.json') as file:
+    data = json.load(file)
+
+list_ = data['personas']
+print(list_)
+
+
+for per in list_ :
+    if re.findall('^cristian',per['nombre']) :
+        print(per)
