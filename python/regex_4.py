29dabbef9478e14ba55d7ec9ce65c26a492fd065
UsuarioTemporal <thomtwd@gmail.com>
2019-02-20 17:22:20 -0500

    regex en json

--- python/regex_4.py
+++ python/regex_4.py
@@ -5,9 +5,15 @@ with open('data.json') as file:
     data = json.load(file)
 
 list_ = data['personas']
-print(list_)
+# print(list_)
 
 
 for per in list_ :
-    if re.findall('^cristian',per['nombre']) :
+    if re.findall('^c',per['nombre']) :
+        print(per)
+
+print('\n')
+
+for per in list_ :
+    if re.findall('an$',per['nombre']):
         print(per)
