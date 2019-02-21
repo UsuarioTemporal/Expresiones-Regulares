ae74662746253f2f558bbf38e16236d59d86bf1d
UsuarioTemporal <thomtwd@gmail.com>
2019-02-20 22:11:26 -0500

    Explicitamente

--- java/Regex_1.java
+++ java/Regex_1.java
@@ -6,9 +6,9 @@ public class Regex_1 {
     public static void main(String[] args) {
         System.out.println(validarDNI("134"));
         System.out.println(validarDNI("asdasdasd"));
-        System.out.println(validarDNI("12345678"));
+        System.out.println(validarDNI("12345678A"));
     }
     static boolean validarDNI(String dni){
-        return dni.matches("[0-9]{8}");
+        return dni.matches("^[0-9]{8}[a-zA-Z]$");
     }
 }
