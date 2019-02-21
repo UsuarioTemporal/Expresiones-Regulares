bf4c77bb6f7114390e9d9d048b347924c926425e
UsuarioTemporal <thomtwd@gmail.com>
2019-02-20 22:05:09 -0500

    validar dni

--- java/Regex_1.java
+++ java/Regex_1.java
@@ -4,6 +4,11 @@
 public class Regex_1 {
 
     public static void main(String[] args) {
-        
+        System.out.println(validarDNI("134"));
+        System.out.println(validarDNI("asdasdasd"));
+        System.out.println(validarDNI("12345678"));
+    }
+    static boolean validarDNI(String dni){
+        return dni.matches("[0-9]{8}");
     }
 }
