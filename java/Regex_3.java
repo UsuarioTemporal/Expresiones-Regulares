3dbfa2af04ba72f8b4577ef284a8092e5dbd05bd
UsuarioTemporal <thomtwd@gmail.com>
2019-02-21 17:10:25 -0500

    Terminando regex en java

--- java/Regex_3.java
+++ java/Regex_3.java
@@ -4,9 +4,15 @@
 public class Regex_3 {
 
     public static void main(String[] args) {
-        
+        System.out.println(validarNombre("Thom"));
+        System.out.println(validarNombre("Thom Maurick"));
+
+        System.out.println(validarEmail("thomtwd@gmail.com"));
     }
     static boolean validarEmail(String texto){
-        return true;
+        return texto.matches("^([\\w-_.])+@[\\w]+\\.[\\w]{2,6}");
+    }
+    static boolean validarNombre(String texto){
+        return texto.matches("^([A-Z]{1}[a-z]{2,}[\\s]?){1,2}$");
     }
 }
