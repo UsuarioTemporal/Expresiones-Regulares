890e3be868b9430983d1cd33d9aa506eed17c3c1
UsuarioTemporal <thomtwd@gmail.com>
2019-02-20 22:00:33 -0500

    Regex en java

---
+++ java/Regex_1.java
@@ -0,0 +1,9 @@
+/**
+ * Regex_1
+ */
+public class Regex_1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- python/regex_6.py
+++ python/regex_6.py
@@ -2,3 +2,6 @@ import re
 
 if re.match('sandra','Sandra',re.IGNORECASE) :
     print('Hecho')
+
+print(re.search(r'hol(a|i)?','holo'))
+print(re.search(r'h(ola)*','holo'))
